2963ec680be91874578e847a1ab71fe0876090d5
Aashish Karki <aashish.y2z@gmail.com>
2018-10-15 11:26:30 +0545

    Add the nepali-date library and then main engine for the date picker

---
+++ CalendarHelper/CalendarConstant.js
@@ -0,0 +1,34 @@
+export const DATE_TYPE_AD = 'A.D';
+export const DATE_TYPE_BS = 'B.S';
+
+export const englishMonth = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'];
+
+export const nepaliMonth = [
+  'Baishakh',
+  'Jestha',
+  'Ashadh',
+  'Shrawan',
+  'Bhadra',
+  'Ashwin',
+  'Kartik',
+  'Mangsir',
+  'Poush',
+  'Magh',
+  'Falgun',
+  'Chaitra'];
+
+export const NUM_OF_WEEKS = 5;
+export const formatDefault = 'YYYY-MM-DD';
+
